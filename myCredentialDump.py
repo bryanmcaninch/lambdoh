705274db25da5243fa92c0d196e3280e11324335
aph3x <bryanmcaninch@users.noreply.github.com>
2017-09-01 21:47:26 -0500

    Delete myCredentialDump.py

--- myCredentialDump.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def lambda_handler(event, context):
-    print "Access Key ID: %s\n" % os.getenv('AWS_ACCESS_KEY_ID')
-    print "Access Key: %s\n" % os.getenv('AWS_SECRET_ACCESS_KEY')
-    print "Security Token: %s\n" % os.getenv('AWS_SECURITY_TOKEN')
-    print "Session Token: %s\n" % os.getenv('AWS_SESSION_TOKEN')
-    print "X-Ray: %s\n" % os.getenv('AWS_XRAY_DAEMON_ADDRESS')
-    return 0
