c2fc98225c38beb776a2f0d4784b4262685ad7d6
aph3x <bryanmcaninch@users.noreply.github.com>
2017-09-01 21:48:27 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -5,5 +5,4 @@ def lambda_handler(event, context):
     print "Access Key: %s\n" % os.getenv('AWS_SECRET_ACCESS_KEY')
     print "Security Token: %s\n" % os.getenv('AWS_SECURITY_TOKEN')
     print "Session Token: %s\n" % os.getenv('AWS_SESSION_TOKEN')
-    print "X-Ray: %s\n" % os.getenv('AWS_XRAY_DAEMON_ADDRESS')
     return 0
